380e251be9a44f3254b25a94dea7893a795e4ab9
thomas <thopan.sakti@gmail.com>
2023-11-05 17:26:31 +0700

    Use dynamic path segments

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("january", views.january),
-    path("february", views.february)
+    path("<month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("Hello World")
-
-def february(request):
-    return HttpResponse("This is February")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Learn Django"
+    elif month == "february":
+        challenge_text = "Do cycling"
+    elif month == "march":
+        challenge_text = "Walk for an hour"
+    else:
+        return HttpResponseNotFound("This month is not supported.")
+    return HttpResponse(challenge_text)
