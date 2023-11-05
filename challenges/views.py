72d4d15585785d7ae7ecff0d21a579602aee9e4e
thomas <thopan.sakti@gmail.com>
2023-11-05 17:37:05 +0700

    Path converters

--- challenges/views.py
+++ challenges/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 # Create your views here.
 
+def monthly_challenge_by_number(request, month):
+    if month in range(1, 13):
+        return HttpResponse(month)
+    else: 
+        return HttpResponseNotFound("Invalid month number.")
 
 def monthly_challenge(request, month):
     challenge_text = None
