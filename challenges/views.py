470e1cd51f45564ddc043f8d1936c33497749390
thomas <thopan.sakti@gmail.com>
2023-11-23 23:24:49 +0700

    Add more dynamic view logic

--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,21 @@ from django.http import HttpResponse, HttpResponseNotFound, Http404
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": "Eat healthy food",
+    "february": "Walk for 20 minutes",
+    "march": "Learn Django",
+    "april": "Do cycling",
+    "may": "Do exercise",
+    "june": "Relax brother",
+    "july": "Eat healthy food",
+    "august": "Walk for 20 minutes",
+    "september": "Learn Django",
+    "october": "Do cycling",
+    "november": "Do exercise",
+    "december": "Relax brother",
+}
+
 def monthly_challenge_by_number(request, month):
     if month in range(1, 13):
         return HttpResponse(month)
@@ -10,13 +25,8 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month number.")
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "Learn Django"
-    elif month == "february":
-        challenge_text = "Do cycling"
-    elif month == "march":
-        challenge_text = "Walk for an hour"
-    else:
-        return HttpResponseNotFound("This month is not supported.")
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("Invalid month!")
